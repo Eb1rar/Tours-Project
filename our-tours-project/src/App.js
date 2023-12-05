121f851869fe78db9655ba2588d7e7f16982a676
ebrar <ebrkdmr1@gmail.com>
2023-12-05 14:43:46 +0300

    continue

--- our-tours-project/src/App.js
+++ our-tours-project/src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Tours from "./Tours";
 import Loading from "./Loading";
 
@@ -7,10 +7,33 @@ const url = "https://course-api.com/react-tours-project";
 function App() {
   const [tours, setTours] = useState([]);
   const [loading, setLoading] = useState(true);
+
+  const fetchTours = async () => {
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      setLoading(false);
+      setTours(data);
+    } catch (error) {
+      setLoading(false);
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchTours();
+  }, []);
+
+  if (loading) {
+    return (
+      <main>
+        <Loading />
+      </main>
+    );
+  }
   return (
     <main>
-      <Tours />
-      <Loading />
+      <Tours tours={tours} />
     </main>
   );
 }
